58d6e0f50eb19f1cb21c6dc1957a2d35d9384fbc
GurgenOhanyan <78640070+GurgenOhanyan@users.noreply.github.com>
2021-04-25 15:11:54 +0400

    Add files via upload

---
+++ ImageDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Images
+{
+    class ImageDownloader
+    {
+        private byte[] imageBytes;
+        private Stopwatch stopwatch = new Stopwatch();
+        public async Task ImageDownloaderAsync(string url)
+        {
+            stopwatch.Start();
+            //Console.WriteLine("Starting image Download...");
+            HttpClient httpClient = new HttpClient();
+            this.imageBytes =await httpClient.GetByteArrayAsync(url);
+            //Console.WriteLine("Succesfully Dolwnloaded image");
+            stopwatch.Stop();
+            Console.WriteLine($"This image took {stopwatch.ElapsedMilliseconds} milliseconds to download");
+        }
+       
+        public void SaveImage(string filePath)
+        {
+            //Console.WriteLine("Saveing image...");
+            stopwatch.Restart();
+            using (Stream fr = new FileStream(filePath, FileMode.Create))
+            {
+                Bitmap bitmap = new Bitmap(RotateImage(this.imageBytes));
+                //fr.Write(this.imageBytes, 0, imageBytes.Length);
+                bitmap.Save(fr, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            //Console.WriteLine("Saved image...");
+            stopwatch.Stop();
+            Console.WriteLine($"This image took {stopwatch.ElapsedMilliseconds} milliseconds to saved");
+        }
+        private  Bitmap ConvertFromBytes(Byte[] imagebytes)
+        {
+           
+            return new Bitmap(new MemoryStream(imagebytes));
+        }
+        private  Bitmap RotateImage(byte[] bytes)
+        {
+            stopwatch.Restart();  
+            Bitmap bitmap = ConvertFromBytes(bytes);
+            
+           bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            stopwatch.Stop();
+            Console.WriteLine($"This image took { stopwatch.ElapsedMilliseconds} milliseconds to rotate");
+           return bitmap;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+namespace Images
+{
+    class Program
+    {
+        private static string directoryPath = @"C:\Users\User\OneDrive\Desktop\images";
+        private const string Default_File_Name = "image.jpg";
+        private static string imagesSourceName = @"C:\Users\User\source\repos\Images\sourceUrls.txt";
+        static async Task Main(string[] args)
+        {
+            var urls = GetImageUrls(imagesSourceName);
+            await MultipleDownloadAsync(urls);
+            Console.ReadKey();
+        }
+
+        static string ExtractImageName(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                return Default_File_Name;
+            }
+
+            int lastIndex = url.LastIndexOf('/');
+            return url.Substring(lastIndex + 1);
+        }
+        static async Task StartDownload(string url)
+        {
+            ImageDownloader imageDownloader = new ImageDownloader();
+            await imageDownloader.ImageDownloaderAsync(url);
+            var di = Directory.CreateDirectory(directoryPath);
+
+            
+            string imageName = ExtractImageName(url);
+
+            string filePath = Path.Combine(di.FullName, imageName);
+     
+            imageDownloader.SaveImage(filePath);
+        }
+        static IEnumerable<string> GetImageUrls(string fileName)
+        {
+            List<string> source = new List<string>();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    source.Add(sr.ReadLine());
+                }
+            }
+
+            return source;
+        }
+
+        private static async Task MultipleDownloadAsync(IEnumerable<string> urls)
+        {
+            ImageDownloader imageDownloader = new ImageDownloader();
+            List<Task> allTasks = new List<Task>();
+            foreach (string url in urls)
+            {
+                Task task = StartDownload(url);
+                allTasks.Add(task);
+            }
+            await Task.WhenAll(allTasks);
+        }
+       
+    }
+}
